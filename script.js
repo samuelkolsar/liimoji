adef150d7171d211e95d223950f12640648d5196
Samuel Kolsar <samuel.kolsar@gmail.com>
2025-02-05 23:24:11 +0200

    Easter egg

---
+++ script.js
@@ -0,0 +1,18 @@
+const lastTrophyDate = new Date('August 1, 2020 19:25:00').getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = now - lastTrophyDate;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+setInterval(updateCountdown, 1000);
